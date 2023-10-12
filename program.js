beed416f91627f1de9ae033d3e4e08682185f06b
Pyrzenq <111853294+Pyrzenq@users.noreply.github.com>
2023-10-12 10:57:31 +0300

    s
    s

---
+++ program.js
@@ -0,0 +1,13 @@
+ika = -0;
+
+if(ika >= 18){
+    console.log("Käyttäjä on täysi-ikäinen.");
+}
+
+if(ika <= 17 && ika >= 0){
+    console.log("Käyttäjä on ala-ikäinen.");
+}
+
+else if(ika <= -1 && ika >= -1){
+    console.log("Virheellinen syöte");
+}
